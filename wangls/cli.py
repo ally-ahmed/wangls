fc0b906e0d3836d67611b8df7ff4f3cbba0c0858
ally-ahmed <norbixin@live.com>
2020-02-04 18:06:21 +0300

    master Added cli functions

--- wangls/cli.py
+++ wangls/cli.py
@@ -1,15 +1,32 @@
 """Console script for wangls."""
 import sys
 import click
+from terminaltables import SingleTable
+
+from wangls.wangls import is_nmap_installed, get_ips_and_os, get_local_ips
 
 
 @click.command()
-def main(args=None):
-    """Console script for wangls."""
-    click.echo("Replace this message by putting your code into "
-               "wangls.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@click.option("--o", "-o", is_flag=True, help="Enable OS detection")
+def main(o):
+    """A command line tool that gives you the IPs of the devices connected to the same network segment."""
+    HEADERS = ["IPs", "MAC Address", "Operating System"]
+    if not is_nmap_installed():
+        click.echo("Nmap is not installed. Please download nmap https://nmap.org/download.html")
+        return
+    if o:
+        os_output = get_ips_and_os()
+        os_output.insert(0, HEADERS)
+        os_table = SingleTable(os_output)
+        os_table.inner_row_border = True
+        click.echo(os_table.table)
+        sys.exit()
+
+    simple_output = get_local_ips()
+    simple_output.insert(0, ["IPs"])
+    simple_table = SingleTable(simple_output)
+    simple_table.inner_row_border = True
+    click.echo(simple_table.table)
 
 
 if __name__ == "__main__":
--- wangls/wangls.py
+++ wangls/wangls.py
@@ -1,1 +1,104 @@
-"""Main module."""
+from halo import Halo
+import socket
+import subprocess
+import os
+import re
+import sys
+import click
+
+
+def get_my_ip():
+    """
+    Find my IP address
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def get_base_ip():
+    """
+    Gets the base ip.
+    :return:
+    """
+    ip_parts = get_my_ip().split(".")
+    base_ip = "{0}.{1}.{2}.".format(*ip_parts)
+    return base_ip
+
+
+@Halo(text="Scanning...", spinner="dots")
+def is_nmap_installed():
+    """Check whether `nmap` is on PATH and marked as executable."""
+
+    from shutil import which
+
+    return which("nmap") is not None
+
+
+@Halo(text="Scanning...", spinner="dots")
+def get_local_ips():
+    """
+      Gets the IP of devices connected to the local network
+    """
+
+    ip_range = f"{get_base_ip()}0/24"
+
+    ips = subprocess.run(
+        f"nmap -sn {ip_range} | grep report | awk '{{print $NF}}'",
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
+    return [[ip] for ip in ips.stdout.strip().split("\n")]
+
+
+def get_ips_and_os():
+    """
+    Gets the IP and OS of devices connected to the local network
+    """
+    # Check if user is root
+    if os.getuid() != 0:
+        click.echo("TCP/IP fingerprinting (for OS scan) requires root privileges. Run sudo wangls -o")
+        sys.exit()
+
+    # my_ip = get_my_ip()
+    ip_range = f"{get_base_ip()}0/24"
+    with Halo(text="Scanning...", spinner="dots"):
+        command_response = subprocess.run(
+            f"nmap -sT -O {ip_range}", shell=True, capture_output=True, text=True
+        )
+
+    response_entries = command_response.stdout.split("\n\n")
+    click.echo(response_entries)
+
+    organized_data = []
+    for i, entry in enumerate(response_entries):
+        if i == len(response_entries) - 1:
+            continue
+        try:
+            ip = re.search(r"(?<=Nmap scan report for)\s.*", entry).group(0).strip()
+        except:
+            ip = "n/a"
+
+        # if ip == my_ip:
+        #     continue
+        try:
+            mac_address = (
+                re.search(r"(?<=:).([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}).*", entry)
+                    .group(0)
+                    .strip()
+            )
+        except:
+            mac_address = "n/a"
+        try:
+            operating_system = re.search(r"(?<=OS details:)\s.*", entry).group(0).strip()
+        except:
+            operating_system = "n/a"
+
+        map_list = [ip, mac_address, operating_system]
+        if ip == "n/a":
+            continue
+        organized_data.append(map_list)
+    return organized_data
